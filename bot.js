4c52635db9a44935517e3dbf8542c438f425282f
Zektric <thezektricedits@gmail.com>
2020-04-15 16:39:57 +0100

    setting up welcome messages

--- bot.js
+++ bot.js
@@ -17,5 +17,13 @@ client.on('message', async msg => {
       msg.reply("Hi, I'm Soul Catcher, the Discord bot developed for Team Damaged Souls! Check out my source code here: https://github.com/TeamDamagedSouls/TDS-Discord-Bot. Contact @AX#1999 if you'd like to help make me better!")
   }
 });
-
+// Create an event listener for new guild members
+client.on('guildMemberAdd', member => {
+  // Send the message to a designated channel on a server:
+  const channel = member.guild.channels.cache.find(ch => ch.name === 'member-log');
+  // Do nothing if the channel wasn't found on this server
+  if (!channel) return;
+  // Send the message, mentioning the member
+  channel.send(`Welcome to the server, ${member}`);
+});
 client.login(auth.token);
