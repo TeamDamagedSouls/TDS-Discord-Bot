e126e41152dcaab3fcfeb3053b0ac1b59c0b9782
arcasoy <arcasoy1@gmail.com>
2020-04-15 14:49:57 +0000

    updating bot.js

--- bot.js
+++ bot.js
@@ -1,14 +1,21 @@
 const Discord = require('discord.js');
+const auth = require('./auth.json');
 const client = new Discord.Client();
 
 client.on('ready', () => {
   console.log(`Logged in as ${client.user.tag}!`);
 });
 
-client.on('message', msg => {
+client.on('message', async msg => {
   if (msg.content === 'ping') {
     msg.reply('pong');
   }
+  else if (msg.content === 'Ping') {
+    msg.reply('Pong');
+  }
+  else if (msg.content === '@Soul Catcher') {
+      msg.reply("Hi, I'm Soul Catcher, the Discord bot developed for Team Damaged Souls! Check out my source code here: https://github.com/TeamDamagedSouls/TDS-Discord-Bot. Contact @AX#1999 if you'd like to help make me better!")
+  }
 });
 
-client.login('token');
+client.login(auth.token);
